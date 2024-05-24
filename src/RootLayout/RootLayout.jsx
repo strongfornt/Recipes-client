8a400f07fd8a58d74cd4a1616dd4555b77064dde
storngfornt <au0038173@gmail.com>
2024-05-24 12:50:21 +0000

    added footer

--- src/RootLayout/RootLayout.jsx
+++ src/RootLayout/RootLayout.jsx
@@ -1,20 +1,23 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../shared/NavBar/Nav";
 import TopHeader from "../shared/NavBar/TopHeader";
-
+import Footer from "../shared/Footer/Footer";
 
 export default function RootLayout() {
   return (
     <>
-        <header>
-            <TopHeader/>
-        </header>
-        <header>
-            <Nav/> 
-        </header>
-        <main>
-                <Outlet/>
-            </main> 
+      <header>
+        <TopHeader />
+      </header>
+      <header>
+        <Nav />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </>
-  )
+  );
 }
